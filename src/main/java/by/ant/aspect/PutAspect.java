aaa35e1c3dec97ad58b255af460b66f1f9716dd6
antonbl32 <antonbl32@users.noreply.github.com>
2021-01-05 20:14:31 +0300

    05/01/2020 part 3

--- src/main/java/by/ant/aspect/PutAspect.java
+++ src/main/java/by/ant/aspect/PutAspect.java
@@ -1,8 +1,8 @@
 package by.ant.aspect;
-
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,8 +10,12 @@ import org.springframework.stereotype.Component;
 public class PutAspect {
 
     @Before("by.ant.aspect.AllAspect.allMethodAdvice() && !by.ant.aspect.AllAspect.allMethodPutAdvice()")
-    public void doPutMethodAdvice(){
+    public void doPutMethodAdvice(JoinPoint joinPoint){
         System.out.println("Do this method without put");
+        MethodSignature methodSignature=(MethodSignature) joinPoint.getSignature();
+        System.out.println(methodSignature.getMethod() + "  "+methodSignature.getReturnType()+"  "+
+                methodSignature.getParameterNames());
+
     }
 //    @Before("by.anton.model.SecondAspect.allMethods()")
 //    public void doAllMethods(){
